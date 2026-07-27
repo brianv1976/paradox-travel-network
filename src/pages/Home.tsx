953b7c7fa85d3261570582dc24f85e7974ed1346
brianv1976 <bvoyles1976@gmail.com>
2026-07-27 17:33:22 -0500

    Remove Reviews component from Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,7 +18,6 @@ import { assets, links } from "../lib/assets";
 import { faqs } from "../data/site";
 import { featuredPosts, categoryImage } from "../data/blog";
 import Globe from "../components/Globe";
-import Reviews from "../components/Reviews";
 import Marquee from "../components/Marquee";
 import Stats from "../components/Stats";
 import SectionHeading from "../components/SectionHeading";
@@ -341,7 +340,6 @@ export default function Home() {
         </div>
       </section>
 
-      <Reviews />
 
       {/* POSTCARDS */}
       <section id="tips" className="container-px py-24 md:py-32">
