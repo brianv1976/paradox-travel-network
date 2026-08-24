c7b37b84812ac53a2713d37d9d781ff57ace75f0
brianv1976 <bvoyles1976@gmail.com>
2026-08-24 16:35:59 -0500

    Make skip-link target focusable [skip netlify]

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,7 +21,7 @@ export default function Layout({ children }: { children: ReactNode }) {
         Skip to main content
       </a>
       <Navbar />
-      <main id="main-content" className="flex-1">
+      <main id="main-content" tabIndex={-1} className="flex-1">
         {reduce ? (
           children
         ) : (
